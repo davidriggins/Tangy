cd8f1e150b856fb6a74504523581ed5297f7a8af
David Riggins <driggins@esri.com>
2022-02-15 18:00:35 -0800

    Section 2: Lesson 15.

--- TangyWeb_Server/Program.cs
+++ TangyWeb_Server/Program.cs
@@ -18,6 +18,11 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+// Use developer exception page
+else
+{
+    app.UseDeveloperExceptionPage();
+}
 
 app.UseHttpsRedirection();
 
