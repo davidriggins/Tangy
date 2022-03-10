54a5214a3bcd72b4bff02a77286d9c07e0cb9c7b
David Riggins <driggins@esri.com>
2022-03-10 10:24:29 -0800

    Section 13: Lesson 118 - Implement product prices in product API.

--- Tangy_Business/Repository/ProductRepository.cs
+++ Tangy_Business/Repository/ProductRepository.cs
@@ -47,7 +47,7 @@ namespace Tangy_Business.Repository
 
         public async Task<ProductDTO> Get(int id)
         {
-            var obj = await _db.Products.Include(u => u.Category).FirstOrDefaultAsync(c => c.Id == id);
+            var obj = await _db.Products.Include(u => u.Category).Include(u=>u.ProductPrices).FirstOrDefaultAsync(c => c.Id == id);
             if (obj != null)
             {
                 return _mapper.Map<Product, ProductDTO>(obj);
@@ -58,7 +58,7 @@ namespace Tangy_Business.Repository
 
         public async Task<IEnumerable<ProductDTO>> GetAll()
         {
-            return _mapper.Map<IEnumerable<Product>, IEnumerable<ProductDTO>>(_db.Products.Include(u => u.Category));
+            return _mapper.Map<IEnumerable<Product>, IEnumerable<ProductDTO>>(_db.Products.Include(u => u.Category).Include(u => u.ProductPrices));
         }
 
         public async Task<ProductDTO> Update(ProductDTO objDTO)
